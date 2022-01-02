c209f6a777872d1ef8a021d2a02b4054cf17971e
Arav <71561508+zCranking@users.noreply.github.com>
2022-01-02 14:26:19 -0800

    Revert "Add files via upload"

--- main.js
+++ main.js
@@ -1,53 +1,70 @@
-Status = "";
-answers = [];
-object = "";
+song1 = "";
+song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rigthWristX = 0;
+rightWristY = 0;
+Status1=null;
+Status2=null;
+scoreLeftWrist = null;
+scoreRightWrist = null;
 function setup(){
-    canvas = createCanvas(600, 470);
-    canvas.position(600, 450);
+    canvas = createCanvas(600, 600);
+    canvas.position(650, 100);
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function gotPoses(results, error){
+    if(error){
+        console.error("shit is going down!");
+    }
+    if(results.length>0){
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist =" + scoreLeftWrist);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("scoreRightWrist = " + scoreRightWrist);
+        //left wrist
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftwristX = " + leftWristX + " leftwristY = " + leftWristY);
+
+        //right wrist
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightwristX = " + rightWristX + " rightwristY = " + rightWristY);
+
+    }
 }
 function modelLoaded(){
     console.log("model loaded");
-    Status = true;
-    objectDetector.detect(video);
 }
-function gotResult(error, results){
-    if(error){
-        console.error(error);
-    }
-    console.log(results);
-    answers = results;
+function preload(){
+    song1 = loadSound("song1.mp3");
+    song2 = loadSound("song2.mp3");
 }
 function draw(){
-    image(video, 0, 0, 600, 470);
-    if(Status != ""){
-        objectDetector.detect(video, gotResult);
-        for(i=0;i<answers.length;i++){
-            document.getElementById("status").innerHTML = "ObjectDetector🔍 has found " + answers.length + " number of objects";
-            fill('green');
-            confidence = floor(answers[i].confidence * 100);
-            text(answers[i].label + " " + confidence + "%", answers[i].x-20, answers[i].y-20);
-            noFill();
-            stroke('green');
-            rect(answers[i].x, answers[i].y, answers[i].width, answers[i].height);
-        }
+    image(video, 0, 0, 0, 600);
+    fill("#FAC748");
+    stroke("#FAC748");
+    Status1=song1.isPlaying();
+    Status2= song2.isPlaying();
+    if(scoreLeftWrist > 0.2){
+    circle(leftWristX, leftWristY,20);
+    setTimeout(song2.stop(), 120);
     }
-}
-function start(){
-    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("status").innerHTML = "Status: Detecting Objects";
-    object = document.getElementById("input").value;
-    console.log(object);
-    if(answers[0].label == object){ 
-        video = "";
-        objectDetector.detect(gotResult);
-        document.getElementById("status").innerHTML = object + " was found.";
-        var synth = window.speechSynthesis;
-        var utterThis = new SpeechSynthesisUtterance(object + "was found.");
-        synth.speak(utterThis);
+    if(Status1==false){
+        song1.setVolume(0.5);
+        setTimeout(song1.play(), 120);
     }
-    else{
-        document.getElementById("status").innerHTML = object + " was not found.";
+    if(scoreRightWrist > 0.2){
+        circle(rightWristX, rightWristY,20);
+        setTimeout(song1.stop(), 120);
+    }
+    if(Status2==false){
+        song2.setVolume(0.5);
+        setTimeout(song2.play(), 120);
     }
 }
